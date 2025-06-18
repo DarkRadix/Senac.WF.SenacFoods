93560bbeba5d93340bc4b8c035d241631e639109
Yuri <radixfire2@gmail.com>
2025-06-17 21:57:40 -0300

    configurações do banco

--- SenacFoodsSolução/SenacFoods/FrmLogin.cs
+++ SenacFoodsSolução/SenacFoods/FrmLogin.cs
@@ -15,21 +15,33 @@ namespace SenacFoods
                 this.Hide();
                 // Criar instancia de FrmPrincipal
                 var frmPrincipal = new FrmPrincipal(txtusuario.Text, txtsenha.Text);
+
                 frmPrincipal.Show();
             }
         }
 
         private bool ValidarLogin(string login, string senha)
         {
-            if (login == "admin" && senha == "123")
+            bool usuarioValido = false;
+            //CONECTA AO BANCO
+            using (var banco = new ComandoDBContext())
             {
-                return true;
+                //CONSULTAR A TABELA USUARIO, SELECT * FROM USUARIOS WHERE EMAIL = ? AND SENHA = ?
+                var usuario = banco.Usuarios.FirstOrDefault(u => u.Email == login && u.Senha == senha);
+
+                if (usuario is not null)
+                    usuarioValido = true;
+
+                if (usuarioValido)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Login ou Senha Inválidos");
+                }
+                return false;
             }
-            else
-            {
-                MessageBox.Show("Login ou Senha Inválidos");
-            }
-            return false;
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
